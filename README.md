a9766b6169f10fe882fec13f60c78ad4658e79a2
cchorley <chris@streeva.com>
2023-11-21 19:43:59 +0000

    Update action to use Atlassian user PAT

--- README.md
+++ README.md
@@ -4,22 +4,3 @@ GitHub Action that locates a Confluence page with a specified title in a specifi
 # Usage
 
 See [action.yml](action.yml)
-
-# Atlassian Access
-This action uses the Atlassian RESTful API using OAuth 2.0 (3LO) authorisation type.
-
-* Create an app at the [Atlassian Developer](https://developer.atlassian.com/) site
-* Go to *Permissions*, choose Configure for Confluence and make sure the following are added:
-  * read:confluence-content.summary
-  * write:confluence-file
-* Go to *Authorization*, chose Configure for OAuth 2.0 (3LO)
-  * Set and copy your **callback URL**
-  * Note down your *Authorization URL* if you want
-* Go to *Settings*:
-  * Copy **Client ID**
-  * Copy **Secret**
-
-This action will obtain an access token from a refresh token, to get a refresh token there is a helper script in this repo which you can use to run through the authorisation flow, it requires the the values copied above:
-```
-./get_access_token <client_id> <secret> <callback_url>
-```
--- attach_report_confluence.sh
+++ attach_report_confluence.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ "$#" -ne 8 ]; then
+if [ "$#" -ne 7 ]; then
   echo "illegal number of parameters"
   exit 1
 fi
@@ -9,22 +9,11 @@ SPACEKEY=$2
 PAGEHEADING=$3
 ATTACHMENT_NAME=$4
 SOURCE_FILE=$5
-CLIENTID=$6
-CLIENTSECRET=$7
-REFRESH_TOKEN=$8
-
-TOKEN_RESPONSE=$(curl -s --request POST \
-  --url 'https://auth.atlassian.com/oauth/token' \
-  --header 'Content-Type: application/json' \
-  --data '{ "grant_type": "refresh_token", "client_id": "'$CLIENTID'", "client_secret": "'$CLIENTSECRET'", "refresh_token": "'$REFRESH_TOKEN'" }')
-
-ACCESS_TOKEN=$(echo $TOKEN_RESPONSE | jq -r .access_token)
-REFRESH_TOKEN=$(echo $TOKEN_RESPONSE | jq -r .refresh_token)
-echo "::set-output name=updatedrefreshtoken::$REFRESH_TOKEN"
+EMAIL=$6
+TOKEN=$7
 
 CQL='cql=title="'$PAGEHEADING'" and Space='$SPACEKEY
-SEARCHRESULTS=$(curl -s -G --url 'https://'$DOMAIN'.atlassian.net/wiki/rest/api/content/search' \
-  --header "Authorization: Bearer $ACCESS_TOKEN" \
+SEARCHRESULTS=$(curl -s -G -u "$EMAIL:$TOKEN" --url 'https://'$DOMAIN'.atlassian.net/wiki/rest/api/content/search' \
   --data-urlencode "$CQL" \
   --data-urlencode 'limit=10' \
   --data-urlencode 'expand=body.storage,version,space')
@@ -32,10 +21,7 @@ SEARCHRESULTS=$(curl -s -G --url 'https://'$DOMAIN'.atlassian.net/wiki/rest/api/
 PAGE_ID=$( echo $SEARCHRESULTS | jq -r .results[].id)
 echo Located page with $PAGE_ID
 
-ATTACHMENTS=$(curl -s \
-  --url 'https://'$DOMAIN'.atlassian.net/wiki/rest/api/content/'$PAGE_ID'/child/attachment?filename='$ATTACHMENT_NAME'&expand=version' \
-  --header "Authorization: Bearer $ACCESS_TOKEN" \
-  --header 'Accept: application/json')
+ATTACHMENTS=$(curl -s -u "$EMAIL:$TOKEN" --url 'https://'$DOMAIN'.atlassian.net/wiki/rest/api/content/'$PAGE_ID'/child/attachment?filename='$ATTACHMENT_NAME'&expand=version' --header 'Accept: application/json')
 
 EXISTING_ATTACHMENTID=$(echo $ATTACHMENTS | jq -r '.results[].id')
 
@@ -43,17 +29,11 @@ if [ -z "$EXISTING_ATTACHMENTID" ]
 then
   # create
   echo "Existing attachment not found"
-  curl -s --request POST \
-    --url 'https://'$DOMAIN'.atlassian.net/wiki/rest/api/content/'$PAGE_ID'/child/attachment' \
-    -H "Authorization: Bearer $ACCESS_TOKEN" \
-    -H "X-Atlassian-Token: no-check" \
-    -F "file=@$SOURCE_FILE;filename=$ATTACHMENT_NAME"
+  URL='https://'$DOMAIN'.atlassian.net/wiki/rest/api/content/'$PAGE_ID'/child/attachment'
 else
   # update/replace
   echo "Existing attachment found"
   URL=https://$DOMAIN.atlassian.net/wiki/rest/api/content/$PAGE_ID/child/attachment/$EXISTING_ATTACHMENTID/data
-  curl -s --request POST --url "$URL" \
-    -H "Authorization: Bearer $ACCESS_TOKEN" \
-    -H "X-Atlassian-Token: nocheck" \
-    -F "file=@$SOURCE_FILE;filename=$ATTACHMENT_NAME"
 fi
+
+curl -s --request POST -u "$EMAIL:$TOKEN" --url "$URL" -H "X-Atlassian-Token: no-check" -F "file=@$SOURCE_FILE;filename=$ATTACHMENT_NAME"
